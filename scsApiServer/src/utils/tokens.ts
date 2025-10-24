e001dbbee023a27057267bfa5ed83027d9c85164
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-10-24 20:04:03 +0530

    refactor: add JSDoc comments for token generation functions to improve code documentation

--- scsApiServer/src/utils/tokens.ts
+++ scsApiServer/src/utils/tokens.ts
@@ -2,6 +2,12 @@ import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 import crypto from 'crypto';
 
+/**
+ * 
+ * @description Generates JWT Access Token
+ * @param tokenPayload object
+ * @returns string (JWT Access Token)
+ */
 export const generateAccessToken=async(tokenPayload:object)=>{
     const token=await jwt.sign(tokenPayload,process.env.ACCESS_TOKEN_SECRET!,{
         expiresIn:'1h'
@@ -9,6 +15,12 @@ export const generateAccessToken=async(tokenPayload:object)=>{
     return token;
 }
 
+/**
+ * 
+ * @description Generates JWT Refresh Token
+ * @param tokenPayload object
+ * @returns string (JWT Refresh Token)
+ */
 export const generateRefreshToken=async(tokenPayload:object)=>{
     const token=await jwt.sign(tokenPayload,process.env.REFRESH_TOKEN_SECRET!,{
         expiresIn:'12h'
@@ -16,6 +28,11 @@ export const generateRefreshToken=async(tokenPayload:object)=>{
     return token;
 }
 
+/**
+ * 
+ * @description Generates a unique Order ID
+ * @returns string (Order ID)
+ */
 export const generateOrderId=async()=>{
     const uniqueId= crypto.randomBytes(16).toString('hex');
     const hash=crypto.createHash('sha256');
