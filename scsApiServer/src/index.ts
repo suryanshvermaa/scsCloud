22b99ad2a35e9ef6ef5a6b25fde30e32f918ab46
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-10-25 00:14:17 +0530

    refactor: update Dockerfile to specify node version and enhance index.ts subdomain routing logic

--- scsApiServer/src/index.ts
+++ scsApiServer/src/index.ts
@@ -20,7 +20,7 @@ const app=express();
 app.get('/*',(req:Request,res:Response,next:NextFunction)=>{
     const hostname=req.hostname;
     const subdomain=hostname.split('.')[0];
-    if(subdomain!='api'){
+    if(hostname!="localhost"&&subdomain!='api'){
         const resolveWeb=`${process.env.BUCKET_HOST_FOR_HOSTING!}/${subdomain}`
         const proxy=httpProxy.createProxy();
         proxy.web(req,res,{target:resolveWeb,changeOrigin:true})
