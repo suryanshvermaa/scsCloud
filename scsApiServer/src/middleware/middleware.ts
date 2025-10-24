ef55bbde0bc6796e3c4ba71a68de920daf412466
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-10-24 19:55:01 +0530

    refactor: streamline JWT authentication middleware for improved readability and error handling

--- scsApiServer/src/middleware/middleware.ts
+++ scsApiServer/src/middleware/middleware.ts
@@ -2,35 +2,25 @@ import { Request,Response,NextFunction } from "express";
 import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 
+/**
+ * 
+ * @description Middleware to verify JWT token from cookies, body, or headers for authentication.
+ * @param req  request object
+ * @param res response object
+ * @param next next middleware function
+ */
 export const authMiddleware=async(req:Request,res:Response,next:NextFunction)=>{
     try {
-        const authCookie=req.cookies.AccessCookie || req.body.AccessCookie;
-        
-        
-        if(authCookie){
-            const isVerified=await jwt.verify(authCookie,process.env.ACCESS_TOKEN_SECRET!);            
-            if(isVerified){                
-                next();
-            }else{
-                res.status(401)
-                .json({
-                    success:false,
-                    message:"unauthorised"
-                })
-            }
-        }
-        else{
-            res.status(401)
-                .json({
-                    success:false,
-                    message:"unauthorised"
-                })
-        }
+        const authCookie=req.cookies.AccessCookie || req.body.AccessCookie || req.headers['accesscookie']|| req.headers.authorization?.split(' ')[1];
+        if(!authCookie) throw new Error("Unauthorised");
+        const isVerified=await jwt.verify(authCookie,process.env.ACCESS_TOKEN_SECRET!); 
+        if(!isVerified) throw new Error("Unauthorised");                           
+        next();
     } catch (error) {
-        res.status(500)
-                .json({
-                    success:false,
-                    message:"Internal server error"
-                })
+        res.status(401)
+            .json({
+                success:false, 
+                message:"Unauthorised"
+            })
     }
 }
