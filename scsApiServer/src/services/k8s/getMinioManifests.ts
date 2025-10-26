449b9e774aabb5132265221874ef050abf29df94
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-10-26 16:03:18 +0530

    feat: Add Minio manifests generation for Kubernetes deployment

---
+++ scsApiServer/src/services/k8s/getMinioManifests.ts
@@ -0,0 +1,184 @@
+import { V1Deployment, V1PersistentVolume, V1Secret, V1Service } from "@kubernetes/client-node";
+
+const getMinioManifests = async (user:string,storageInGB:number,accesskey:string,secretkey:string) => {
+    const deployment:V1Deployment={
+        apiVersion: "apps/v1",
+        kind: "Deployment",
+        metadata: {
+            name: "minio-deployment"+user,
+            labels: {
+                app: "minio"+user
+            },
+        },
+        spec: {
+            replicas: 1,
+            selector: {
+                matchLabels: {
+                    app: "minio"+user
+                },
+            },
+            template: {
+                metadata: {
+                    labels: {
+                        app: "minio"+user
+                    },
+                },
+                spec: {
+                    containers: [
+                        {
+                            name: "minio",
+                            image: "minio/minio",
+                            args: ["server", "/data"],
+                            ports: [
+                                {
+                                    containerPort: 9000,
+                                },
+                            ],
+                            env: [
+                                {
+                                    name: "MINIO_ROOT_USER",
+                                    valueFrom:{
+                                        secretKeyRef:{
+                                            name: "minio-secret"+user,
+                                            key: "accesskey"
+                                        }
+                                    },
+                                },
+                                {
+                                    name: "MINIO_ROOT_PASSWORD",
+                                    valueFrom:{
+                                        secretKeyRef:{
+                                            name: "minio-secret"+user,
+                                            key: "secretkey"
+                                        }
+                                    },
+                                },
+                            ],
+                            volumeMounts:[
+                                {
+                                    name: "minio-storage",
+                                    mountPath: "/data",
+                                }
+                            ]
+                        },
+
+                    ],
+                    volumes:[
+                        {
+                            name: "minio-storage",
+                            persistentVolumeClaim:{
+                                claimName: "minio-pvc"+user
+                            }
+                        }
+                    ]
+                },
+            },
+        },
+    };
+
+    const persistentVolume:V1PersistentVolume={
+        apiVersion: "v1",
+        kind: "PersistentVolume",
+        metadata: {
+            name: "minio-pv"+user,
+        },
+        spec: {
+            capacity: {
+                storage: `${storageInGB}Gi`
+                },
+            volumeMode: "Filesystem",
+            accessModes: ["ReadWriteOnce"],
+            persistentVolumeReclaimPolicy: "Retain",
+            hostPath: {
+                path: `/mnt/minio/${user}`
+            },
+        },
+    }
+
+    const persistentVolumeClaim={
+        apiVersion: "v1",
+        kind: "PersistentVolumeClaim",
+        metadata: {
+            name: "minio-pvc"+user,
+        },
+        spec: {
+            accessModes: ["ReadWriteOnce"],
+            resources: {
+                requests: {
+                    storage: `${storageInGB}Gi`
+                },
+            },
+        },
+    }
+
+    const secrets: V1Secret={
+        apiVersion: "v1",
+        kind: "Secret",
+        metadata: {
+            name: "minio-secret"+user,
+        },
+        type: "Opaque",
+        stringData: {
+            accesskey: accesskey,
+            secretkey: secretkey,
+        },
+    }
+
+    const service={
+        apiVersion: "v1",
+        kind: "Service",
+        metadata: {
+            name: "minio-service"+user,
+            namespace: "minio",
+        },
+        spec: {
+            selector: {
+                app: "minio"+user
+            },
+            ports: [
+                {
+                    port: 9000,
+                    targetPort: 9000
+                }
+            ],
+            type: "ClusterIP"
+        },
+    }
+
+    const ingress={
+        apiVersion: "networking.k8s.io/v1",
+        kind: "Ingress",
+        metadata: {
+            name: "minio-ingress"+user,
+            namespace: "minio",
+            labels: {
+                "app.kubernetes.io/name": "minio-ingress"+user,
+                "app.kubernetes.io/part-of": "minio"
+            }
+        },
+        spec: {
+            rules: [
+                {
+                    host: `minio-${user}.${process.env.HOSTING_DOMAIN!}`,
+                    http: {
+                        paths: [
+                            {
+                                pathType: "Prefix",
+                                path: "/",
+                                backend: {
+                                    service: {
+                                        name: "minio-service"+user,
+                                        port: {
+                                            number: 9000
+                                        }
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    };
+    return {deployment,persistentVolume,persistentVolumeClaim,secrets,service,ingress};
+};
--- scsApiServer/src/services/k8s/k8s.ts
+++ scsApiServer/src/services/k8s/k8s.ts
@@ -5,3 +5,5 @@ kc.loadFromDefault();
 
 export const k8sbatchAPi = kc.makeApiClient(k8s.BatchV1Api);
 export const k8sObjectApi = k8s.KubernetesObjectApi.makeApiClient(kc);
+export const k8sCoreV1Api = kc.makeApiClient(k8s.CoreV1Api);
+export const k8sCoreApi = kc.makeApiClient(k8s.CoreApi);
