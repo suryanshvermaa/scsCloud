d82d00641f0f83a597b8b795d401e92b91325bda
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-10-27 15:36:14 +0530

    refactor: Remove unused useMemo import from AmountScreen component

--- scscloud/src/pages/AmountScreen.tsx
+++ scscloud/src/pages/AmountScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import axios from "axios";
