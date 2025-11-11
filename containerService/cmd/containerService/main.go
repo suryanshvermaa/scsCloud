91475db2fa582a3ee944167c0df60436646cc9b3
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-11-11 12:02:50 +0530

    feat: Enhance main.go with dotenv support and retry logic for repository initialization

--- containerService/cmd/containerService/main.go
+++ containerService/cmd/containerService/main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
+	"log"
+	"time"
+
+	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
+	containerservice "github.com/suryanshvermaa/scsCloud/containerService"
+	"github.com/tinrab/retry"
 )
 
 type Config struct {
 	DatabaseURL string `envconfig:"DATABASE_URL"`
+	Port        int    `envconfig:"PORT" default:"8080"`
 }
 
 func main() {
+	// for development
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("No .env file found")
+	}
+
 	var cfg Config
-	err := envconfig.Process("", &cfg)
+	err = envconfig.Process("", &cfg)
 	if err != nil {
 		panic(err)
 	}
+	var r containerservice.Repository
+	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
+		r, err = containerservice.NewPostgresRepository(cfg.DatabaseURL)
+		if err != nil {
+			log.Println(err)
+		}
+		return nil
+	})
+	defer r.Close()
+	log.Println("Listening on port", cfg.Port)
+	s := containerservice.NewService(r)
+	log.Fatal(containerservice.ListenGRPC(s, cfg.Port))
 }
