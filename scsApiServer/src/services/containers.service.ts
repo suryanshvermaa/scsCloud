15ac7c7d903f5d607ab74c2a7ef79393a6b58c92
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-11-14 02:34:14 +0530

    feat: Refactor job scheduling to use worker queues and remove unused ECS job configurations

--- scsApiServer/src/services/containers.service.ts
+++ scsApiServer/src/services/containers.service.ts
@@ -1,10 +1,5 @@
 import { scheduleHostingJob, scheduleTranscodingJob } from "./k8s.service";
 
-export const queueHost = process.env.QUEUE_HOST;
-export const queueUser = process.env.QUEUE_USER;
-export const queuePassword = process.env.QUEUE_PASSWORD;
-export const myBucketName = process.env.MY_BUCKET_NAME;
-
 export interface IRunProps {
   videoKey: string;
   userAccessKey: string;
@@ -25,7 +20,6 @@ export interface IRunPropsHost {
  * @param {IRunProps} runObj
  */
 export const spinTranscoder = async (runObj: IRunProps) => {
-    // await transcodingContainerTask(runObj);
     await scheduleTranscodingJob(runObj);
 };
 
@@ -34,6 +28,5 @@ export const spinTranscoder = async (runObj: IRunProps) => {
  * @param {IRunPropsHost} runObj 
  */
 export const spinHoster = async (runObj: IRunPropsHost) => {
-//  await hostingContainerTask(runObj);
     await scheduleHostingJob(runObj);
 };
