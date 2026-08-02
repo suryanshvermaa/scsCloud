fce0d371ed2e45415100e0b4b7f188353595bffc
“suryansh <suryanshv.ug23.ee@nitp.ac.in>
2026-08-02 22:42:50 +0530

    style: format code and update storage endpoints to HTTPS

--- scsApiServer/src/services/objectStorage/methods.ts
+++ scsApiServer/src/services/objectStorage/methods.ts
@@ -1,14 +1,14 @@
-import { S3Client, PutObjectCommand, ListBucketsCommand, GetObjectCommand, DeleteObjectCommand, CreateBucketCommand, DeleteBucketCommand, ListObjectsCommand, PutBucketCorsCommand  } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, ListBucketsCommand, GetObjectCommand, DeleteObjectCommand, CreateBucketCommand, DeleteBucketCommand, ListObjectsCommand, PutBucketCorsCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import getMinioManifests from "../k8s/getMinioManifests";
 import { k8sObjectApi } from "../k8s/k8s";
 import mime from 'mime-types'
 
-interface IS3ClientConfig{
+interface IS3ClientConfig {
     region: string;
-    endpoint:string;
-    accessKeyId:string;
-    secretAccessKey?:string;
+    endpoint: string;
+    accessKeyId: string;
+    secretAccessKey?: string;
 }
 
 /**
@@ -19,34 +19,34 @@ interface IS3ClientConfig{
  * @param secretkey secretkey for Minio
  * @returns {StorageEndpoint:string} endpoint
  */
-const enableBucketService=async(userId:string,storageInGB:number,accesskey:string,secretkey:string)=>{
-    const base64encodedSecretKey=Buffer.from(secretkey,'utf-8').toString('base64');
-    const base64encodedAccessKey=Buffer.from(accesskey,'utf-8').toString('base64');
-    const userIdSanitized=userId.replace(/[^a-z0-9]/gi,'n').toLowerCase();//k8s resource name sanitization
-    const {deployment,persistentVolume,persistentVolumeClaim,secrets,service,ingress}=getMinioManifests(userIdSanitized,storageInGB,base64encodedAccessKey,base64encodedSecretKey);
+const enableBucketService = async (userId: string, storageInGB: number, accesskey: string, secretkey: string) => {
+    const base64encodedSecretKey = Buffer.from(secretkey, 'utf-8').toString('base64');
+    const base64encodedAccessKey = Buffer.from(accesskey, 'utf-8').toString('base64');
+    const userIdSanitized = userId.replace(/[^a-z0-9]/gi, 'n').toLowerCase();//k8s resource name sanitization
+    const { deployment, persistentVolume, persistentVolumeClaim, secrets, service, ingress } = getMinioManifests(userIdSanitized, storageInGB, base64encodedAccessKey, base64encodedSecretKey);
     await k8sObjectApi.create(secrets); //secret
     await k8sObjectApi.create(persistentVolume);//pv
     await k8sObjectApi.create(persistentVolumeClaim); // pvc
     await k8sObjectApi.create(deployment); // deployment
     await k8sObjectApi.create(service); // service
     await k8sObjectApi.create(ingress); // ingress
-    return {StorageEndpoint:`http://minio-${userIdSanitized}.${process.env.HOSTING_DOMAIN!}`,serviceName:`minio-service-${userIdSanitized}`,ingressEndpoint:`http://minio-${userIdSanitized}.${process.env.HOSTING_DOMAIN!}`};
-} 
+    return { StorageEndpoint: `https://minio-${userIdSanitized}.${process.env.HOSTING_DOMAIN!}`, serviceName: `minio-service-${userIdSanitized}`, ingressEndpoint: `https://minio-${userIdSanitized}.${process.env.HOSTING_DOMAIN!}` };
+}
 
 /**
  * @description get S3 Client instance
  * @param s3ClientConfig 
  * @returns 
  */
-const getS3Client=(s3ClientConfig:IS3ClientConfig):S3Client=>{
+const getS3Client = (s3ClientConfig: IS3ClientConfig): S3Client => {
     return new S3Client({
-        region:s3ClientConfig.region!,
-        endpoint:s3ClientConfig.endpoint!,
-        credentials:{
-            accessKeyId:s3ClientConfig.accessKeyId!,
-            secretAccessKey:s3ClientConfig.secretAccessKey!
+        region: s3ClientConfig.region!,
+        endpoint: s3ClientConfig.endpoint!,
+        credentials: {
+            accessKeyId: s3ClientConfig.accessKeyId!,
+            secretAccessKey: s3ClientConfig.secretAccessKey!
         },
-        forcePathStyle:true,
+        forcePathStyle: true,
     })
 }
 
@@ -56,7 +56,7 @@ const getS3Client=(s3ClientConfig:IS3ClientConfig):S3Client=>{
  * @param bucketName 
  * @param prefix 
  */
-const listObjects=async(s3Client:S3Client,bucketName:string,prefix?:string)=>{
+const listObjects = async (s3Client: S3Client, bucketName: string, prefix?: string) => {
     const command = new ListObjectsCommand({
         Bucket: bucketName,
         Prefix: prefix
@@ -68,7 +68,7 @@ const listObjects=async(s3Client:S3Client,bucketName:string,prefix?:string)=>{
  * @description list all buckets
  * @param s3Client 
  */
-const listBuckets=async(s3Client:S3Client)=>{
+const listBuckets = async (s3Client: S3Client) => {
     return (await s3Client.send(new ListBucketsCommand())).Buckets;
 }
 
@@ -79,14 +79,14 @@ const listBuckets=async(s3Client:S3Client)=>{
  * @param objectKey 
  * @param contentType 
  */
-const putObjectSignedUrl=async(s3Client:S3Client,bucketName:string,objectKey:string,expiresIn?:number)=>{
+const putObjectSignedUrl = async (s3Client: S3Client, bucketName: string, objectKey: string, expiresIn?: number) => {
     const contentType = mime.lookup(objectKey)
     const command = new PutObjectCommand({
         Bucket: bucketName,
         Key: objectKey,
-        ContentType: contentType||"",
+        ContentType: contentType || "",
     });
-    return await getSignedUrl(s3Client,command,{expiresIn:3600});
+    return await getSignedUrl(s3Client, command, { expiresIn: 3600 });
 }
 
 /**
@@ -95,15 +95,15 @@ const putObjectSignedUrl=async(s3Client:S3Client,bucketName:string,objectKey:str
  * @param bucketName 
  * @param objectKey 
  */
-const getObjectSignedUrl=async(s3Client:S3Client,bucketName:string,objectKey:string)=>{
+const getObjectSignedUrl = async (s3Client: S3Client, bucketName: string, objectKey: string) => {
     const command = new GetObjectCommand({
         Bucket: bucketName,
         Key: objectKey
     });
-    return await getSignedUrl(s3Client,command,{expiresIn:3600})
+    return await getSignedUrl(s3Client, command, { expiresIn: 3600 })
 }
 
-const deleteObject=async(s3Client:S3Client,bucketName:string,objectKey:string)=>{
+const deleteObject = async (s3Client: S3Client, bucketName: string, objectKey: string) => {
     const command = new DeleteObjectCommand({
         Bucket: bucketName,
         Key: objectKey
@@ -111,14 +111,14 @@ const deleteObject=async(s3Client:S3Client,bucketName:string,objectKey:string)=>
     return await s3Client.send(command);
 }
 
-const createS3Bucket=async(s3Client:S3Client,bucketName:string)=>{
+const createS3Bucket = async (s3Client: S3Client, bucketName: string) => {
     const command = new CreateBucketCommand({
         Bucket: bucketName,
     });
     return await s3Client.send(command);
 }
 
-const deleteS3Bucket=async(s3Client:S3Client,bucketName:string)=>{
+const deleteS3Bucket = async (s3Client: S3Client, bucketName: string) => {
     const command = new DeleteBucketCommand({
         Bucket: bucketName,
     });
